896f7c46c4ee30f0befdaecdc8932fac94139c46
Simón Gutiérrez Brida <sgutierrez@dc.exa.unrc.edu.ar>
2022-04-18 10:08:52 -0300

    Finished with EvoSuite script (maybe?)

--- runEvoSuite.sh
+++ runEvoSuite.sh
@@ -18,26 +18,24 @@ OPTIONS=t:,s:,b:,e:,c:,f:,h
 
 #Display script usage
 #error : if 0 the usage comes from normal behaviour, if > 0 then it comes from an error and will exit with this as exit code
+#extra information : an additional message
 function usage() {
     local code="$1"
+    local additionalMsg="$2"
     local msg="Runs EvoSuite for a particular class and a given configuration for EvoSuite, then it runs JaCoCo to meassure line and branch coverage.\nUsage:\nrunEvoSuite.sh -[-h]elp to show this message\nrunEvoSuite.sh -[-t]argetClassname <target> -[-s]ourceDir <path> -[-b]inDir <path> -[-]t[e]stDir <path> -[-c]lasspath <paths> -[-]con[f]igFile <path>\n\tTarget class is a full classname.\n\tSource and Bin paths refers to where the sources (.java) and compiled (.class) files are located respectivelly.\n\tThe classpath refers to additional paths needed, these must be separated by ':'.\n\tThe config file refers to a .evoconfig file with the EvoSuite configuration to use (see example.evoconfig)."
     if [[ "$code" -eq "0" ]]; then
+        [ ! -z "$additionalMsg" ] && infoMessage "$additionalMsg"
         infoMessage "$msg"
         exit 0
     else
-        error "Wrong usage\n$msg" "$code"
+        if [ -z "$additonalMsg" ]; then
+            error "Wrong usage\n$msg" "$code"
+        else
+            error "Wrong usage\n${additionalMsg}\n$msg" "$code"
+        fi
     fi
 }
 
-evoArguments=""
-evoProperties=""
-parseFromConfigFile "test.evoconfig" "=" " " "#" "[[:lower:]]" "--" evoArguments
-parseFromConfigFile "test.evoconfig" "=" "=" "#" "D" "-" evoProperties
-
-echo "evoArguments = $evoArguments"
-echo "evoProperties = $evoProperties"
-
-exit 0
 
 #Arguments
 classname=""
@@ -50,32 +48,102 @@ testDir=""
 testDirSet=0
 additionalClasspath=""
 additionalClasspathSet=0
+configFile=""
+configFileSet=0
 
 
+while true; do
+	case "$1" in
+		--targetClassname | -t)
+			classname="$2"
+			[ -z "$classname" ] || $(echo "$classname" | egrep -q "^[[:space:]]+$") && error "key,value separator is empty or contains only spaces" 2
+			classnameSet=1
+			shift 2
+		;;
+		--sourceDir | -s)
+			sourceDir="$2"
+			[ -z "$sourceDir" ] || $(echo "$sourceDir" | egrep -q "^[[:space:]]+$") && error "source directory path (${sourceDir}) is empty or contains only spaces" 3
+			[ ! -d "$sourceDir" ] && error "source directory ($sourceDir) does not exists or is not a directory" 3
+			sourceDirSet=1
+			shift 2
+		;;
+		--binDir | -b)
+			binDir="$2"
+			[ -z "$binDir" ] || $(echo "$binDir" | egrep -q "^[[:space:]]+$") && error "bin directory path (${binDir}) is empty or contains only spaces" 4
+			[ ! -d "$binDir" ] && error "bin directory ($binDir) does not exists or is not a directory" 4
+			binDirSet=1
+			shift 2
+		;;
+		--testDir | -e)
+			testDir="$2"
+			[ -z "$testDir" ] || $(echo "$testDir" | egrep -q "^[[:space:]]+$") && error "tests directory path (${testDir}) is empty or contains only spaces" 5
+			[ -d "$testDir" ] && [ ! -z "$(ls -A ${testDir})" ] && error "tests directory exists and is not empty" 5 
+			testDirSet=1
+			shift 2
+		;;
+		--classpath | -c)
+			additionalClasspath="$2"
+			[ -z "$additionalClasspath" ] || $(echo "$additionalClasspath" | egrep -q "^[[:space:]]+$") && error "additional classpath (${additionalClasspath}) is empty or contains only spaces" 6
+			$(echo "$additionalClasspath" | egrep -q "(^:.*)|(.*:$)") && error "additional classpath ($additionalClasspath) cannot start or end with ':'" 6
+			additionalClasspathSet=1
+			shift 2
+		;;
+		--configFile | -f)
+			additionalClasspath="$2"
+			[ -z "$configFile" ] || $(echo "$configFile" | egrep -q "^[[:space:]]+$") && error "config file path (${configFile}) is empty or contains only spaces" 7
+            $(echo "$configFile" | egrep -qv ".*\.evoconfig$") && error "config file ($configFile) does not have extension '.evoconfig'" 7
+            [ ! -f "$configFile" ] && error "config file (${configFile}) does not exists" 7
+			configFileSet=1
+			shift 2
+		;;
+		--help | -h)
+			usage 0 ""
+		;;
+		--)
+			shift
+			break
+		;;
+		*)
+			echo "Invalid arguments"
+			exit 3
+		;;
+	esac
+done
 
 
-example="$1"
-if [ "$example" == "bad" ]; then
-	classname="motivating.queue.BadQueue"
-	testDir="tests/queuebest/bad/"
-elif [ "$example" == "normal" ]; then
-	classname="motivating.queue.Queue"
-	testDir="tests/queuebest/normal/"
-elif [ "$example" == "good" ]; then
-	classname="motivating.queue.GoodQueue"
-	testDir="tests/queuebest/good/"
-elif [ "$example" == "goodWithRep" ]; then
-	classname="motivating.queue.GoodQueueWithRep"
-	testDir="tests/queuebest/goodWithRep/"
-else 
-	error "Bad option: $example" -1
+classnameSet=0
+sourceDir=""
+sourceDirSet=0
+binDir=""
+binDirSet=0
+testDir=""
+testDirSet=0
+additionalClasspath=""
+additionalClasspathSet=0
+configFile=""
+configFileSet=0
+
+[[ "$classnameSet" -ne "1" ]] && usage 8 "Classname was not set"
+[[ "$sourceDirSet" -ne "1" ]] && usage 8 "Source directory was not set"
+if [[ "$binDirSet" -ne "1" ]]; then
+    binDir="$sourceDir"
+    binDirSet=1
+    warning "No binary directory set, will be using source directory instead"
 fi
-sourceDir="benchmarks/src/"
-binDir="benchmarks/bin/"
+[[ "$testDirSet" -ne "1" ]] && usage 8 "Tests directory was not set"
+[[ "$additionalClasspathSet" -ne "1" ]] && debug "No additional classpath was set"
+[[ "$configFileSet" -ne "1" ]] && usage 8 "Configuration file was not set"
+
+infoMessage "Parsing EvoSuite configuration from ${configFile} ..."
+
+evoArguments=""
+evoProperties=""
+parseFromConfigFile "${configFile}" "=" " " "#" "[[:lower:]]" "--" evoArguments
+parseFromConfigFile "${configFile}" "=" "=" "#" "D" "-" evoProperties
+
+debug "EvoSuite configuration parsed\nArguments: ${evoArguments}\nProperties: ${evoProperties}"
+
 classnameAsPath=$(echo "$classname" | sed 's;\.;/;g')
-budget="10"  #Time budget for evosuite (in seconds)
-seed="42"
-evoCriterion="branch:weakmutation"
 #
 #Constants
 CURRENT_DIR=$(pwd)
@@ -99,20 +167,16 @@ JACOCO_REPORT="JaCoCoRS/JaCoCoRS.sh"
 #class 			: class for which to generate tests
 #project classpath 	: classpath to project related code and libraries
 #output dir		: where tests will be placed
-#criterion 		: the criteria that evosuite will use to evolve the test suite (weakmutation, strongmutation, branch, etc)
-#budget 		: the time budget (in seconds) for evosuite
-#seed 			: the seed to be used for the random generator
+#argumentsAndProperties :   arguments and properties for EvoSuite (excluding Dtest_dir and Djunit_suffix)
 function evosuite() {
 	infoMessage "Running EvoSuite..."
 	#-base_dir $outputDir 
 	local class="$1"
 	local projectCP="$2"
 	local outputDir="$3"
-	local criterion="$4"
-	local budget="$5"
-	local seed="$6"
-	debug "Running cmd: java -jar $EVOSUITE_JAR -class $class -projectCP $projectCP -Dtest_dir=$outputDir -criterion $criterion -Djunit_suffix=$ES_JUNIT_SUFFIX -Dsearch_budget=$budget -seed $seed"
-	java -jar $EVOSUITE_JAR -class $class -projectCP $projectCP -Dtest_dir="$outputDir" -criterion $criterion -Djunit_suffix="$ES_JUNIT_SUFFIX" -Dsearch_budget=$budget -seed $seed
+	local argumentsAndProperties="$4"
+	debug "Running cmd: java -jar $EVOSUITE_JAR -class $class -projectCP $projectCP -Dtest_dir=$outputDir -Djunit_suffix=$ES_JUNIT_SUFFIX ${argumentsAndProperties}"
+	java -jar $EVOSUITE_JAR -class $class -projectCP $projectCP -Dtest_dir="$outputDir" -Djunit_suffix="$ES_JUNIT_SUFFIX" ${argumentsAndProperties}
 }
 
 #Compiles evosuite generated tests
@@ -174,6 +238,7 @@ function jacoco() {
 	local classToAnalyzeAsPath=$(echo "$classToAnalyze" | sed 's|\.|/|g')
 	local fullPathToClassToAnalize=""
 	local fullPathToSourceOfClassToAnalize=""
+	local JUNIT_RUNNER=org.junit.runner.JUnitCore
 	appendPaths "$classfiles" "${classToAnalyzeAsPath}.class" 0 fullPathToClassToAnalize
 	appendPaths "$sourcefiles" "${classToAnalyzeAsPath}.java" 0 fullPathToSourceOfClassToAnalize
 	if [[ "$USE_OFFLINE_INSTRUMENTATION" -eq "1" ]]; then
@@ -196,7 +261,7 @@ function jacoco() {
 		fi
 		debug "Running tests with instrumented class"
 		debug "Running cmd: java -cp $classpath:$testpath:$JUNIT:$HAMCREST:$TESTING_JARS_ES:$JACOCO_AGENT -Djacoco-agent.destfile=${OFFLINE_EXEC_FILE_LOCATION} org.junit.runner.JUnitCore $tests"
-		java -cp "$classpath:$testpath:$JUNIT:$HAMCREST:$TESTING_JARS_ES:$JACOCO_AGENT:$OFFLINE_INSTR_DIR_LOCATION" -Djacoco-agent.destfile=${OFFLINE_EXEC_FILE_LOCATION} org.junit.runner.JUnitCore $tests
+		java -cp "$classpath:$testpath:$JUNIT:$HAMCREST:$TESTING_JARS_ES:$JACOCO_AGENT:$OFFLINE_INSTR_DIR_LOCATION" -Djacoco-agent.destfile=${OFFLINE_EXEC_FILE_LOCATION} $JUNIT_RUNNER $tests
 		exitCode="$?"
 		debug "Restoring original file $fullPathToClassToAnalize"
 		mv "${fullPathToClassToAnalize}.bak" "$fullPathToClassToAnalize"
@@ -210,7 +275,7 @@ function jacoco() {
 	else
 		infoMessage "Using Java Agent approach"
 		debug "Running cmd: java -javaagent: $JACOCO_AGENT -cp $classpath:$testpath:$JUNIT:$HAMCREST:$TESTING_JARS_ES org.junit.runner.JUnitCore $tests"
-		java -javaagent:"$JACOCO_AGENT" -cp "$classpath:$testpath:$JUNIT:$HAMCREST:$TESTING_JARS_ES" org.junit.runner.JUnitCore $tests
+		java -javaagent:"$JACOCO_AGENT" -cp "$classpath:$testpath:$JUNIT:$HAMCREST:$TESTING_JARS_ES" $JUNIT_RUNNER $tests
 		exitCode="$?"
 		if [[ "$exitCode" -ne "0" ]]; then
 		 error "Failed to run tests with JaCoCo agent (${exitCode})" 501

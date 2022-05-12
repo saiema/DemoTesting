cac225c93b26a1ffd9d3a561160bdadfd2c3a5de
Simón Gutiérrez Brida <sgutierrez@dc.exa.unrc.edu.ar>
2022-05-12 19:09:55 -0300

    Improved colored printing to avoid some weird bugs

--- clean.sh
+++ clean.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+testsFolder="$1"
+
 [ -e "evosuite-report" ] && rm -rf "evosuite-report"
-if [ -e "tests" ]; then
-	pushd "tests"
-	ls | xargs -I {} rm -rf {}
-	popd
+if [ -z "$testsFolder" ] || $(echo "$testsFolder" | egrep -q "^[[:space:]]$"); then
+    [ -e "evosuite-tests" ] && rm -rf "evosuite-tests"
+else
+    if [ -e "$testsFolder" ]; then
+	    pushd "$testsFolder"
+	    ls | xargs -I {} rm -rf {}
+	    popd
+    fi
 fi
 [ -e "instrumentedCode" ] && rm -rf "instrumentedCode"
 [ ! -z  "$(find . -name "jacoco.*")" ] && rm jacoco.*
+[ -e "evosuite.log" ] && rm "evosuite.log"

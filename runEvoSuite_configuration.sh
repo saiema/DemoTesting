f3fa6da96afdce550971b674676526bdef8e2b65
Simón Gutiérrez Brida <sgutierrez@dc.exa.unrc.edu.ar>
2022-05-16 19:27:56 -0300

    Added several hardcoded filenames

--- runEvoSuite_configuration.sh
+++ runEvoSuite_configuration.sh
@@ -7,6 +7,7 @@ CURRENT_DIR=$(pwd)
 EVOSUITE_JAR="${CURRENT_DIR}/tools/evosuite/evosuite-fb.jar" #USE THIS FOR EVOSUITE-FB
 EVOSUITE_ADDITIONAL_FLAGS="-generateSuite" #This is required when using evosuite-fb
 EVOSUITE_LOG="evosuite.log"
+EVOSUITE_SCRIPT_LOG="runEvoSuite.log"
 JUNIT="${CURRENT_DIR}/tools/junit-4.12.jar"
 HAMCREST="${CURRENT_DIR}/tools/org.hamcrest.core_1.3.0.v201303031735.jar"
 #TESTING_JARS_ES="${CURRENT_DIR}/tools/evosuite/evosuite-standalone-runtime-1.0.6.jar" #USE THIS FOR EVOSUITE 1.0.6
@@ -14,7 +15,8 @@ TESTING_JARS_ES="${CURRENT_DIR}/tools/evosuite/evosuite-fb-runtime.jar" #USE THI
 ES_JUNIT_SUFFIX="ESTest"	#EvoSuite test suffix
 #JACOCO
 USE_OFFLINE_INSTRUMENTATION=1 #0 : do not use offline instrumentation; 1 : use offline instrumentation
-OFFLINE_EXEC_FILE_LOCATION="jacoco.exec"
+JACOCO_EXEC_FILE="jacoco.exec"
+JACOCO_REPORT_RESUMED_FILE="jacoco.report.resumed"
 OFFLINE_INSTR_DIR_LOCATION="instrumentedCode"
 JACOCO_AGENT="JaCoCoRS/jacocoagent.jar"
 JACOCO_CLI="JaCoCoRS/jacococli.jar"
